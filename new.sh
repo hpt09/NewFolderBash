57742923e9ccd601a38c14eb9b4d307dae481bc5
Harpreet Singh <hpt09@live.com>
2017-09-07 15:43:28 +1200

    Adding echo comment

--- new.sh
+++ new.sh
@@ -5,6 +5,7 @@ read -p "Enter a name for the new creation: " NAME
         #checking if directory/creation exist
         while [ -d "$path" ] ; do
 	echo "Creation already exists"
+	echo "You need to enter a different name"
         read -p "Enter a name for the new creation: " NAME
         path=./$NAME
         done
